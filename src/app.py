b76b124ca0cd0326a075ed80c6345b6619b9bb82
M4rky07 <juniorgomes22919@gmail.com>
2026-08-31 11:02:14 -0300

    chore: configura projeto inicial

---
+++ src/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify
+import json
+
+app = Flask(__name__)
+
+with open("instituicoesensino.json", "r", encoding="utf-8") as arquivo:
+    instituicoes = json.load(arquivo)
+
+
+@app.get("/")
+def index():
+    return {
+        "versao": "1.0.0",
+        "status": "ok",
+        "aviso": "to aqui"
+    }, 200
+
+
+@app.get("/instituicoesensino")
+def instituicoesensino():
+    return jsonify(instituicoes), 200
+
+
+@app.get("/instituicoesensino/<int:id>")
+def instituicao_por_id(id):
+
+    for instituicao in instituicoes:
+        if instituicao["co_entidade"] == id:
+            return jsonify(instituicao), 200
+
+    return jsonify({
+        "erro": "Instituição não encontrada"
+    }), 404
+
+
+@app.get("/instituicoesensino/<string:nome>")
+def instituicao_por_nome(nome):
+    for instituicao in instituicoes:
+        if instituicao["no_entidade"].lower() == nome.lower():
+            return jsonify(instituicao), 200
+
+    return jsonify({
+        "erro": "Instituição não encontrada"
+    }), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
